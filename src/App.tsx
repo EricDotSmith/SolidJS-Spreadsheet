bb18edb485d3cf4ec3d35b0e6ce8fd44c2817293
Eric Smith <esmith11235@gmail.com>
2023-06-17 18:40:00 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { type Component } from "solid-js";
+
+const App: Component = () => {
+  return <Sheet />;
+};
+
+const Sheet: Component = () => {
+  return <div class="min-h-fit">hello</div>;
+};
